04aaadc616ac9cfefd17b68501a9307760e858b4
ZhaoMengjia <840381567@qq.com>
2016-03-13 20:42:41 +0800

    258. Add Digits

---
+++ 002. Add Digits/addDigits.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int addDigits(int num) {
+        if (num < 10){
+            return num;
+        }
+        else{
+            return addDigits(num%10 + addDigits(num/10));
+        }
+    }
+}
